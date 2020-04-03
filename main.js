bb7d002d4edbecfd7b2f95386f107a043033de9a
Adrian De Oleo comas <adriandoc1999@gmail.com>
2020-04-03 18:46:25 -0400

    Effects Added

--- main.js
+++ main.js
@@ -70,59 +70,71 @@ function pieceOne(){
     position5.innerHTML = position1.innerHTML
     position5.style.visibility = "visible"
     position1.style.visibility = "hidden"
+    position1.innerHTML = 0
     counter()
+    Verification()
   }
   if (position2.style.visibility == "hidden"){
     position2.innerHTML = position1.innerHTML
     position2.style.visibility = "visible"
     position1.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
 
 function pieceTwo(){
 
+
   if (position1.style.visibility == "hidden" ){
     position1.innerHTML = position2.innerHTML
     position1.style.visibility = "visible"
     position2.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position3.style.visibility == "hidden"){
     position3.innerHTML = position2.innerHTML
     position3.style.visibility = "visible"
     position2.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position6.style.visibility == "hidden"){
     position6.innerHTML = position2.innerHTML
     position6.style.visibility = "visible"
     position2.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
 
 function pieceThree(){
 
+
   if (position2.style.visibility == "hidden" ){
     position2.innerHTML = position3.innerHTML
     position2.style.visibility = "visible"
     position3.style.visibility = "hidden"
     counter()
+    Verification()
+
   }
   if (position4.style.visibility == "hidden"){
     position4.innerHTML = position3.innerHTML
     position4.style.visibility = "visible"
     position3.style.visibility = "hidden"
     counter()
-  }
+    Verification()
+}
   if (position7.style.visibility == "hidden"){
     position7.innerHTML = position3.innerHTML
     position7.style.visibility = "visible"
     position3.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
@@ -133,62 +145,75 @@ function pieceFourth(){
     position3.style.visibility = "visible"
     position4.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position8.style.visibility == "hidden"){
     position8.innerHTML = position4.innerHTML
     position8.style.visibility = "visible"
     position4.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
 function pieceFive(){
+
+ 
 
   if (position1.style.visibility == "hidden" ){
     position1.innerHTML = position5.innerHTML
     position1.style.visibility = "visible"
     position5.style.visibility = "hidden"
     counter()
+    Verification()
+
   }
   if (position6.style.visibility == "hidden"){
     position6.innerHTML = position5.innerHTML
     position6.style.visibility = "visible"
     position5.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position9.style.visibility == "hidden"){
     position9.innerHTML = position5.innerHTML
     position9.style.visibility = "visible"
     position5.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
 function pieceSix(){
+ 
 
   if (position2.style.visibility == "hidden" ){
     position2.innerHTML = position6.innerHTML
     position2.style.visibility = "visible"
     position6.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position7.style.visibility == "hidden"){
     position7.innerHTML = position6.innerHTML
     position7.style.visibility = "visible"
     position6.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position5.style.visibility == "hidden"){
     position5.innerHTML = position6.innerHTML
     position5.style.visibility = "visible"
     position6.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position10.style.visibility == "hidden"){
     position10.innerHTML = position6.innerHTML
     position10.style.visibility = "visible"
     position6.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
@@ -198,24 +223,28 @@ function pieceSeven(){
     position3.style.visibility = "visible"
     position7.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position6.style.visibility == "hidden"){
     position6.innerHTML = position7.innerHTML
     position6.style.visibility = "visible"
     position7.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position8.style.visibility == "hidden"){
     position8.innerHTML = position7.innerHTML
     position8.style.visibility = "visible"
     position7.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position11.style.visibility == "hidden"){
     position11.innerHTML = position7.innerHTML
     position11.style.visibility = "visible"
     position7.style.visibility = "hidden"
     counter()
+    Verification()
   }
 }
 function pieceEight(){
@@ -225,18 +254,21 @@ function pieceEight(){
     position4.style.visibility = "visible"
     position8.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position7.style.visibility == "hidden"){
     position7.innerHTML = position8.innerHTML
     position7.style.visibility = "visible"
     position8.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position12.style.visibility == "hidden"){
     position12.innerHTML = position8.innerHTML
     position12.style.visibility = "visible"
     position8.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
@@ -247,18 +279,21 @@ function pieceNine(){
     position5.style.visibility = "visible"
     position9.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position10.style.visibility == "hidden"){
     position10.innerHTML = position9.innerHTML
     position10.style.visibility = "visible"
     position9.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position13.style.visibility == "hidden"){
     position13.innerHTML = position9.innerHTML
     position13.style.visibility = "visible"
     position9.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
@@ -268,24 +303,29 @@ function pieceTen(){
     position6.style.visibility = "visible"
     position10.style.visibility = "hidden"
     counter()
+    Verification()
+    
   }
   if (position9.style.visibility == "hidden"){
     position9.innerHTML = position10.innerHTML
     position9.style.visibility = "visible"
     position10.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position11.style.visibility == "hidden"){
     position11.innerHTML = position10.innerHTML
     position11.style.visibility = "visible"
     position10.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position14.style.visibility == "hidden"){
     position14.innerHTML = position10.innerHTML
     position14.style.visibility = "visible"
     position10.style.visibility = "hidden"
     counter()
+    Verification()
   }
 }
 function pieceEleven(){
@@ -294,24 +334,28 @@ function pieceEleven(){
     position7.style.visibility = "visible"
     position11.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position12.style.visibility == "hidden"){
     position12.innerHTML = position11.innerHTML
     position12.style.visibility = "visible"
     position11.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position10.style.visibility == "hidden"){
     position10.innerHTML = position11.innerHTML
     position10.style.visibility = "visible"
     position11.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position15.style.visibility == "hidden"){
     position15.innerHTML = position11.innerHTML
     position15.style.visibility = "visible"
     position11.style.visibility = "hidden"
     counter()
+    Verification()
   }
 }
 
@@ -321,24 +365,28 @@ function pieceTwelve(){
     position8.style.visibility = "visible"
     position12.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position11.style.visibility == "hidden"){
     position11.innerHTML = position12.innerHTML
     position11.style.visibility = "visible"
     position12.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position16.style.visibility == "hidden"){
     position16.innerHTML = position12.innerHTML
     position16.style.visibility = "visible"
     position12.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position14.style.visibility == "hidden"){
     position14.innerHTML = position10.innerHTML
     position14.style.visibility = "visible"
     position10.style.visibility = "hidden"
     counter()
+    Verification()
   }
 }
 function pieceThirteen(){
@@ -348,12 +396,14 @@ function pieceThirteen(){
     position9.style.visibility = "visible"
     position13.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position14.style.visibility == "hidden"){
     position14.innerHTML = position13.innerHTML
     position14.style.visibility = "visible"
     position13.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
@@ -363,18 +413,21 @@ function pieceFourteen(){
     position10.style.visibility = "visible"
     position14.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position13.style.visibility == "hidden"){
     position13.innerHTML = position14.innerHTML
     position13.style.visibility = "visible"
     position14.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position15.style.visibility == "hidden"){
     position15.innerHTML = position14.innerHTML
     position15.style.visibility = "visible"
     position14.style.visibility = "hidden"
     counter()
+    Verification()
   }
 }
 function pieceFifteen(){
@@ -383,18 +436,21 @@ function pieceFifteen(){
     position11.style.visibility = "visible"
     position15.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position14.style.visibility == "hidden"){
     position14.innerHTML = position15.innerHTML
     position14.style.visibility = "visible"
     position15.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position16.style.visibility == "hidden"){
     position16.innerHTML = position15.innerHTML
     position16.style.visibility = "visible"
     position15.style.visibility = "hidden"
     counter()
+    Verification()
   }
 }
 function pieceSixteen(){
@@ -404,17 +460,20 @@ function pieceSixteen(){
     position12.style.visibility = "visible"
     position16.style.visibility = "hidden"
     counter()
+    Verification()
   }
   if (position15.style.visibility == "hidden"){
     position15.innerHTML = position16.innerHTML
     position15.style.visibility = "visible"
     position16.style.visibility = "hidden"
     counter()
+    Verification()
   }
 
 }
 
 function play(){
+  document.getElementById('image').style.display = 'none'
   if(moveNumber != 0 ||  document.getElementById("hms").innerHTML != "00:00:00" ){
     reiniciar()
   }
@@ -474,16 +533,20 @@ while(aNumeroAleatorios.length < cantidadNumeros ){
   position1.style.visibility = "visible"
 
   
-
+  Verification()
 }
 
 function ready(){
+
+
   if(position1.innerHTML == 1 && position2.innerHTML == 2 && position3.innerHTML == 3 && position4.innerHTML == 4
     && position5.innerHTML == 5 && position6.innerHTML == 6 && position7.innerHTML == 7 && position8.innerHTML == 8
     && position9.innerHTML == 9 && position10.innerHTML == 10 && position11.innerHTML ==  11 && position12.innerHTML == 12 
     && position13.innerHTML == 13 && position14.innerHTML == 14 && position15.innerHTML == 15 && position16.style.visibility == "hidden"  ){
     document.getElementById("alert").innerHTML = "Felicidades! Haz logrado resolver este Puzzle en solo " + moveNumber + " movimientos en un tiempo de " +  document.getElementById("hms").innerHTML + " seg"
       parar()
+      document.getElementById('image').style.display = 'inline'
+      
     
   }else{
     document.getElementById("alert").innerHTML = "El Puzzle necesita ser completado"
@@ -497,10 +560,144 @@ function noPossible(){
     && position13.innerHTML == 13 && position14.innerHTML == 15 && position15.innerHTML == 14 && position16.style.visibility == "hidden"  ){
     document.getElementById("alert").innerHTML = "Felicidades! Acabas de demostrar que este puzzle al igual que otros no puede ser resuelto" 
     parar()
-    
-  }else{
+    document.getElementById('image').style.display = 'inline'
+     } else{
     document.getElementById("alert").innerHTML = "Mantente Intentando, este Puzzle puede ser completado"
+   }
+ 
   }
+
+function Verification(){
+
+if(position1.innerHTML == 1){
+    position1.style.backgroundColor = 'rgb(230, 230, 230)';
+    position1.style.color= 'black'
+  }else{
+   position1.style.backgroundColor = '#00000050';
+   position1.style.color= 'white'
+  }
+
+  if(position2.innerHTML == 2){
+    position2.style.backgroundColor = 'rgb(230, 230, 230)';
+    position2.style.color= 'black'
+  }else{
+   position2.style.backgroundColor = '#00000050';
+   position2.style.color= 'white'
+  }
+
+  if(position3.innerHTML == 3){
+    position3.style.backgroundColor = 'rgb(230, 230, 230)';
+    position3.style.color= 'black'
+  }else{
+   position3.style.backgroundColor = '#00000050';
+   position3.style.color= 'white'
+  }
+
+  if(position4.innerHTML == 4){
+    position4.style.backgroundColor = 'rgb(230, 230, 230)';
+    position4.style.color= 'black'
+  }else{
+   position4.style.backgroundColor = '#00000050';
+   position4.style.color= 'white'
+  }
+  
+     if(position5.innerHTML == 5){
+      position5.style.backgroundColor = 'rgb(230, 230, 230)';
+      position5.style.color= 'black'
+    }else{
+     position5.style.backgroundColor = '#00000050';
+     position5.style.color= 'white'
+    }
+
+  if(position6.innerHTML == 6){
+    position6.style.backgroundColor = 'rgb(230, 230, 230)';
+    position6.style.color= 'black'
+  }else{
+   position6.style.backgroundColor = '#00000050';
+   position6.style.color= 'white'
+  }
+
+  if(position7.innerHTML == 7){
+    position7.style.backgroundColor = 'rgb(230, 230, 230)';
+    position7.style.color= 'black'
+  }else{
+   position7.style.backgroundColor = '#00000050';
+   position7.style.color= 'white'
+  }
+  
+  if(position8.innerHTML == 8){
+    position8.style.backgroundColor = 'rgb(230, 230, 230)';
+    position8.style.color= 'black'
+  }else{
+   position8.style.backgroundColor = '#00000050';
+   position8.style.color= 'white'
+  }
+
+if(position9.innerHTML == 9){
+  position9.style.backgroundColor = 'rgb(230, 230, 230)';
+  position9.style.color= 'black'
+}else{
+ position9.style.backgroundColor = '#00000050';
+ position9.style.color= 'white'
+}
+
+if(position10.innerHTML == 10){
+  position10.style.backgroundColor = 'rgb(230, 230, 230)';
+  position10.style.color= 'black'
+}else{
+ position10.style.backgroundColor = '#00000050';
+ position10.style.color= 'white'
+}
+
+if(position11.innerHTML == 11){
+  position11.style.backgroundColor = 'rgb(230, 230, 230)';
+  position11.style.color= 'black'
+}else{
+ position11.style.backgroundColor = '#00000050';
+ position11.style.color= 'white'
+}
+
+if(position12.innerHTML == 12){
+  position12.style.backgroundColor = 'rgb(230, 230, 230)';
+  position12.style.color= 'black'
+}else{
+ position12.style.backgroundColor = '#00000050';
+ position12.style.color= 'white'
+}
+
+if(position13.innerHTML == 13){
+  position13.style.backgroundColor = 'rgb(230, 230, 230)';
+  position13.style.color= 'black'
+}else{
+ position13.style.backgroundColor = '#00000050';
+ position13.style.color= 'white'
+}
+
+if(position14.innerHTML == 14){
+  position14.style.backgroundColor = 'rgb(230, 230, 230)';
+  position14.style.color= 'black'
+}else{
+ position14.style.backgroundColor = '#00000050';
+ position14.style.color= 'white'
+}
+
+if(position15.innerHTML == 15){
+  position15.style.backgroundColor = 'rgb(230, 230, 230)';
+  position15.style.color= 'black'
+}else{
+ position15.style.backgroundColor = '#00000050';
+ position15.style.color= 'white'
+}
+
+if(position16.innerHTML == 16){
+  position16.style.backgroundColor = 'rgb(230, 230, 230)';
+  position16.style.color= 'black'
+}else{
+ position16.style.backgroundColor = '#00000050';
+ position16.style.color= 'white'
+
+}
+
 }
 
 
